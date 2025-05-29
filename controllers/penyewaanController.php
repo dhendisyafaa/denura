1a604cac4c3084384baf391acd74c3db621c76d4
Aura Putri K <aura232kireina@gmail.com>
2025-05-29 20:07:08 +0700

    add feat/barang-jasa-penyewaan

---
+++ controllers/penyewaanController.php
@@ -0,0 +1,34 @@
+<?php
+include __DIR__ . '/../config/conn.php';
+
+function getAllPenyewaan() 
+{
+    global $conn;
+    $query = "SELECT * FROM penyewaan";
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        error_log("Query failed: " . mysqli_error($conn));
+        return [];
+    }
+
+    $penyewaan = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $penyewaan[] = $row;
+    }
+
+    return $penyewaan;
+}
+
+function getPenyewaanById($id)
+{
+    global $conn;
+    $query = "SELECT * FROM penyewaan WHERE idSewa = ?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $penyewaan = mysqli_fetch_assoc($result);
+    mysqli_stmt_close($stmt);
+    return $penyewaan;
+}
