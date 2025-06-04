aaa2d1a9face28769a06d31937451fe7a6332a81
Dhendi Syafa Athallah Putra <91307794+dhendisyafaa@users.noreply.github.com>
2025-06-04 12:57:00 +0700

    Squashed commit of the following:
    commit ab36efc80d6882782a98254424e11e822053299a
    Merge: 29f854d a59633e
    Author: Dhendi Syafa Athallah Putra <91307794+dhendisyafaa@users.noreply.github.com>
    Date:   Tue Jun 3 23:42:49 2025 +0700
    
        Merge pull request #6 from dhendisyafaa/feat/barang-jasa-penyewaan
    
        tambah fitur jasa
    
    commit a59633e3931f21415cf7e877936efeb993b20271
    Author: Aura Putri K <aura232kireina@gmail.com>
    Date:   Tue Jun 3 14:40:09 2025 +0700
    
        tambah fitur jasa

--- modules/jasa/jasaService.php
+++ modules/jasa/jasaService.php
@@ -10,3 +10,30 @@ function getJasaByIdService($id)
 {
   return getJasaByIdRepo($id);
 }
+
+function createJasaService($id_user, $jenis_jasa)
+{
+  // Validasi idUser
+  if (!is_numeric($id_user) || $id_user <= 0) {
+    return false;
+  }
+
+  return insertJasaRepo($id_user, $jenis_jasa);
+}
+
+function updateJasaService($id_jasa, $id_user, $jenis_jasa)
+{
+  // Validasi idJasa dan idBarang
+  if (!is_numeric($id_jasa) || $id_jasa <= 0 || !is_numeric($id_user) || $id_user <= 0) {
+    return false;
+  }
+
+  return updateJasaRepo($id_jasa, $id_user, $jenis_jasa);
+}
+
+function deleteJasaService($id_jasa)
+{
+  if (!is_numeric($id_jasa) || $id_jasa <= 0)
+    return false;
+  return deleteJasaRepo($id_jasa);
+}
